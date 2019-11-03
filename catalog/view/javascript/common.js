d1b09eef39d39d6d96e7eb6a7df69eda9475ae3f
Minh Tuan <minhtuan9922@gmail.com>
2019-11-03 12:00:58 +0700

    cap nhat

--- catalog/view/javascript/common.js
+++ catalog/view/javascript/common.js
@@ -100,7 +100,7 @@ $(document).ready(function() {
 		if (cols == 2) {
 			$('#content .product-list').attr('class', 'product-layout product-grid col-lg-6 col-md-6 col-sm-12 col-12');
 		} else if (cols == 1) {
-			$('#content .product-list').attr('class', 'product-layout product-grid col-lg-4 col-md-4 col-sm-6 col-12');
+			$('#content .product-list').attr('class', 'product-layout product-grid col-lg-3 col-md-4 col-sm-6 col-6 col-p-5');
 		} else {
 			$('#content .product-list').attr('class', 'product-layout product-grid col-lg-3 col-md-3 col-sm-6 col-12');
 		}
